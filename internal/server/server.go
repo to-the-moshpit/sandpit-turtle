9dd87d7aa2877e4de1e556968f49a95fa2c98e25
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-09-26 16:16:19 +0200

    feat: integrate logger package

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,24 +8,23 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/to-the-moshpit/sandpit-turtle/internal/database"
 	"github.com/to-the-moshpit/sandpit-turtle/internal/environment"
+	"github.com/to-the-moshpit/sandpit-turtle/internal/logger"
 )
 
 type Server struct {
-	port int
-
-	db database.Service
+	db  database.Service
+	log logger.Logger
 }
 
 func NewServer(env *environment.Env) *http.Server {
 	NewServer := &Server{
-		port: env.Port,
-
-		db: database.New(env),
+		db:  database.New(env),
+		log: logger.New(env),
 	}
 
 	// Declare Server config
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Addr:         fmt.Sprintf(":%d", env.Port),
 		Handler:      NewServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
